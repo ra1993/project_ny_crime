23145497f995128d429d221c0c9747c2b7ccd090
ra1993 <richardamin93@gmail.com>
2022-04-25 09:44:03 -0400

    git push --all --tags --forceAdding .gitignore removing credentials

--- g_drive_dir/g_drive_main.py
+++ g_drive_dir/g_drive_main.py
@@ -9,9 +9,24 @@ credentials_file_path = '/home/ra-terminal/Desktop/projects/project_ny_crime/g_d
 #define api scope
 SCOPE = 'https://www.googleapis.com/auth/drive'
 
-# define store
-store = file.Storage(credentials_file_path)
-credentials = store.get()# get access token
-if not credentials or credentials.invalid:
-    flow = client.flow_from_clientsecrets(clientsecret_file_path, SCOPE)
-    credentials = tools.run_flow(flow, store)
+def get_cred_access(client_secret_file_path, credentials_file_path):
+    # define store
+    store = file.Storage(credentials_file_path)
+    credentials = store.get()# get access token
+    
+    try:
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(clientsecret_file_path, SCOPE)
+            credentials = tools.run_flow(flow, store)
+            # print("Successful Connection:","Flow:",flow, "Creds:",credentials)
+    except GoogleDriveConnection.Error:
+        print("Unable to get credential access to drive api")
+    else:
+        print("Successful Connection: Creds:",credentials)
+        return credentials
+
+    
+if __name__ == "__main__":
+    credentials = get_cred_access(client_secret_file_path, credentials_file_path)
+    print(credentials)
+    pass
